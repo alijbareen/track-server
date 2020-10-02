acb222835ad7b506b17be8ea3bfbc406865e4f19
alijb <alijb@post.bgu.ac.il>
2020-10-02 17:48:03 +0300

    Setted Up postMan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri =
   "mongodb+srv://admin:passwordpassword@cluster0.l2vyv.mongodb.net/<dbname>?retryWrites=true&w=majority";
